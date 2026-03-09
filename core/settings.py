0f90e295b929f58222826ccbe16a7f8ddc09b481
Muhammadmuso <berzloyme@gmail.com>
2026-03-09 15:29:09 +0500

    t

--- core/settings.py
+++ core/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-agsby+w(j_sqw8xw$foknooblc(=ka22w7)5sc0omk9$h8rzm_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
